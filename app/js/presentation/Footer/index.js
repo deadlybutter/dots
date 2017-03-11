d51d0a4993ea49a87546d0bbeb818b5640607893
deadlybutter <thedeadlybutter@gmail.com>
2017-03-11 18:55:48 -0500

    Adding footer data

--- app/js/presentation/Footer/index.js
+++ app/js/presentation/Footer/index.js
@@ -7,6 +7,7 @@ const Footer = (props) => {
     <footer className="footer row end-xs">
       <div className="col-xs-12">
         <p>created by <a href="https://twitter.com/itsjoekent" target="_blank">joe kent</a></p>
+        <p>contribute to the project on <a href="https://github.com/deadlybutter/dots">github</a></p>
       </div>
     </footer>
   );
