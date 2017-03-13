8a5c197e0670e6c30d91a090296fff30a0d0e1d9
deadlybutter <thedeadlybutter@gmail.com>
2017-03-13 11:16:54 -0400

    Commiting progress from last night

--- data/trump-russia.js
+++ data/trump-russia.js
@@ -31,6 +31,18 @@ module.exports = {
     {
       title: 'Russian Oligarch',
       background: '#001f3f',
+    },
+    {
+      title: 'Company',
+      background: '#3D9970',
+    },
+    {
+      title: 'Kazakh',
+      background: '#FF851B',
+    },
+    {
+      title: 'White House Inner Circle',
+      background: '#7FDBFF',
     }
   ],
   nodes: [
@@ -83,6 +95,66 @@ module.exports = {
       title: 'Serhiy Lovochkin',
       description: `Serhiy Lovochkin is a Ukrainian politician, Member of the Parliament of Ukraine, Deputy Head of the Opposition Bloc faction in Verhovna Rada and Mr. Yanukovych’s former chief of staff.`,
       categories: ['Opposition Bloc'],
+    },
+    {
+      title: 'Donald Trump Jr.',
+      description: 'Oldest child of Donald Trump, works alongside his brother as a trustee of The Trump Organization',
+      categories: ['Team Trump'],
+    },
+    {
+      title: 'Donald Trump',
+      description: 'American businessman, television personality, politician, and the 45th President of the United States.',
+      categories: ['Team Trump'],
+    },
+    {
+      title: 'Bayrock Group LLC',
+      description: `New York real estate development company that surfaced in the early 2000s and, by 2014, had all but disappeared except for a few lawsuits.`,
+      categories: ['Company', 'Organized Crime'],
+    },
+    {
+      title: 'Tevfik Arif',
+      description: 'Bayrock’s former Chairman',
+      categories: ['Organized Crime', 'Kazakh'],
+    },
+    {
+      title: 'Felix Sater',
+      description: `The "Number 2 man" at Bayrock`,
+      categories: ['Organized Crime', 'Team Trump'],
+    },
+    {
+      title: 'Alexander Mashkevich',
+      description: `Strategic partner to Bayrock and a major shareholder in ENRC, a miner that was delisted from the London Stock Exchange in 2013 amid corruption allegations.`,
+      categories: ['Kazakh', 'Organized Crime'],
+    },
+    {
+      title: 'Patokh Chodiev',
+      description: `Major shareholder in ENRC, a miner that was delisted from the London Stock Exchange in 2013 amid corruption allegations. In the 1990s he established the Chodiev Group, joining with Michael Cherney, who the FBI believed was a major Russian organized-crime figure. (Listed as Kazakh due to close business proximity)`,
+      categories: ['Organized Crime', 'Kazakh']
+    },
+    {
+      title: 'Alijan Ibragimov',
+      description: `Major shareholder in ENRC, a miner that was delisted from the London Stock Exchange in 2013 amid corruption allegations.`,
+      categories: ['Kazakh'],
+    },
+    {
+      title: 'The Trump Organization',
+      description: ` American privately owned international conglomerate based in Trump Tower in Midtown Manhattan, New York City. It comprises the business ventures and investments of Donald Trump. From 1971 to 2017, Trump ran the company as chairman and president.`,
+      categories: ['Company'],
+    },
+    {
+      title: 'Ivanka Trump',
+      description: 'American businesswoman, former reality TV personality, socialite, author, and fashion model. She is the daughter of U.S. President Donald Trump and his first wife, former model Ivana Trump.',
+      categories: ['Team Trump', 'White House Inner Circle']
+    },
+    {
+      title: 'FL Group',
+      description: 'Strategic financing partner to Bayrock until it went under',
+      categories: ['Company'],
+    },
+    {
+      title: 'Tamir Sapir',
+      description: `American businessman and investor from the former Soviet republic of Georgia who made millions bartering fertilizer and oil with the Soviets in the 1980s; he took most of his money and put it into New York real-estate. This profile makes a decent argument he was Russian Intelligence http://washingtonmonthly.com/2017/02/20/trumps-soho-project-the-mob-and-russian-intelligence/`,
+      categories: ['Russian Intelligence'],
     }
   ],
   dots: [
@@ -164,6 +236,109 @@ module.exports = {
       date: '8/14/2016',
       nodes: ['Paul Manafort', 'Viktor Yanukovych'],
       sources: ['https://www.nytimes.com/2016/08/15/us/politics/paul-manafort-ukraine-donald-trump.html']
-    }
+    },
+    {
+      title: 'Donald Trump Jr. talks about Russian assets at a conference',
+      description: `"And in terms of high-end product influx into the US, Russians make up a pretty disproportionate cross-section of a lot of our assets; say in Dubai, and certainly with our project in SoHo and anywhere in New York. We see a lot of money pouring in from Russia. There's indeed a lot of money coming for new-builds and resale reflecting a trend in the Russian economy and, of course, the weak dollar versus the ruble," he said.`,
+      date: '9/15/2008',
+      nodes: ['Donald Trump Jr.'],
+      sources: ['http://www.eturbonews.com/5008/executive-talk-donald-trump-jr-bullish-russia-and-few-emerging-ma'],
+    },
+    {
+      title: 'Bayrock is founded',
+      description: 'The company is founded by Tevfik Arif, eventually moving to the 24th floor of Trump Tower. (Exact date unknown)',
+      date: '7/21/2001',
+      nodes: ['Tevfik Arif'],
+      sources: ['https://en.wikipedia.org/wiki/Bayrock_Group', 'http://imgur.com/a/AWPD3'],
+    },
+    {
+      title: 'Felix Sater joins Bayrock',
+      description: `Felix Sater has testified that he joined Bayrock, controlled by Tevfik Arif, around 2002. (Exact date unknown)`,
+      date: '1/1/2002',
+      nodes: ['Bayrock Group LLC', 'Tevfik Arif', 'Felix Sater'],
+      sources: ['https://www.forbes.com/sites/richardbehar/2016/10/03/donald-trump-and-the-felon-inside-his-business-dealings-with-a-mob-connected-hustler/#edd5c4a22824'],
+    },
+    {
+      title: 'Donald Trump gives Bayrock exclusive rights to a project in Moscow',
+      description: `According to Sater's 2008 deposition. "I'd come back, pop my head into Mr. Trump's office and tell him, you know, 'Moving forward on the Moscow deal.' And he would say 'All right.'" "I showed him photos, I showed him the site, showed him the view from the site. It's pretty spectacular." (Exact date unknown)`,
+      date: '1/1/2005',
+      nodes: ['Bayrock Group LLC', 'Donald Trump', 'Felix Sater', 'The Trump Organization'],
+      sources: ['https://www.forbes.com/sites/richardbehar/2016/10/03/donald-trump-and-the-felon-inside-his-business-dealings-with-a-mob-connected-hustler/#edd5c4a22824'],
+    },
+    {
+      title: 'Bayrock announces plans for Trump International Hotel & Residence',
+      description: 'First joint project between Bayrock and The Trump Organization (Exact date unknown)',
+      date: '1/1/2003',
+      nodes: ['Bayrock Group LLC', 'Donald Trump', 'The Trump Organization'],
+      sources: [
+        'https://en.wikipedia.org/wiki/Bayrock_Group#Trump_International_Hotel_.26_Residence',
+        'https://en.wikipedia.org/wiki/Trump_International_Hotel_%26_Residence',
+      ],
+    },
+    {
+      title: 'Trump SoHo Launch Party',
+      description: `After the eventual completion of the hotel Trump invited multiple family members and business parters. Note: Source clearly shows him with Tevfik Arif and Felix Sater.`,
+      date: '9/19/2007',
+      nodes: ['Bayrock Group LLC', 'Donald Trump', 'Donald Trump Jr.', 'Felix Sater', 'Tevfik Arif', 'The Trump Organization', 'Ivanka Trump', 'Tamir Sapir'],
+      sources: [
+        'http://www.gettyimages.com/event/trump-soho-launch-party-inside-76839333#donald-trump-tevfik-arif-and-felix-sater-attend-the-trump-soho-launch-picture-id110093705',
+        'http://www.gettyimages.com/event/trump-soho-launch-party-inside-76839333#tamir-sapir-attends-the-trump-soho-launch-party-picture-id83540250',
+      ],
+    },
+    {
+      title: 'NYT Runs profile on Slater exposing his past',
+      description: `Not long after the Trump SoHo launch, The New York Times runs a story exposing Felix Slater's past crimes.`,
+      date: '12/7/2007',
+      nodes: ['Felix Slater'],
+      sources: ['http://www.nytimes.com/2007/12/17/nyregion/17trump.html'],
+    },
+    {
+      title: 'Slater leaves Bayrock',
+      description: `"He was involved. No one disputes his involvement at the early stages. But after early 2008 he was no longer affiliated with the company." (Exact date unknown)`,
+      date: '1/1/2008',
+      nodes: ['Felix Slater'],
+      sources: ['http://www.telegraph.co.uk/news/2016/05/26/exclusive-russian-mob-linked-fraudster-a-key-player-in-donald-tr/'],
+    },
+    {
+      title: 'Slater becomes senior advisor to Donald Trump',
+      description: `For about a year Sater became a "Senior Advisor to Donald Trump" and was given a Trump Organization e-mail address, business cards and an office there. The phone number listed on his business card previously belonged to a lawyer in Trump's general counsel's office. (Exact date unknown)`,
+      date: '1/1/2010',
+      nodes: ['Felix Slater', 'Donald Trump', 'The Trump Organization'],
+      sources: ['https://www.forbes.com/sites/richardbehar/2016/10/03/donald-trump-and-the-felon-inside-his-business-dealings-with-a-mob-connected-hustler/#edd5c4a22824'],
+    },
+    {
+      title: 'Lawsuit filed against Bayrock for money laundering is unsealed',
+      description: `A Manhattan judge has unsealed a civil tax fraud case brought in behalf of New York State against associates of Donald Trump. The suit accuses convicted racketeer and Felix Sater, Bayrock Group, and prominent law firms of conspiring to launder as much as $250 million dollars of profit on Trump projects Bayrock was co-developing out of the country in a scheme to evade $100 million dollars of state and federal income tax.`,
+      date: '7/14/2016',
+      nodes: ['Felix Slater', 'Donald Trump', 'The Trump Organization', 'Bayrock Group LLC', 'Tevfik Arif'],
+      sources: [
+        'http://www.prnewswire.com/news-releases/ny-attorney-general-green-lights-250-million-tax-fraud-prosecution-of-trump-projects-300298935.html',
+        'https://www.documentcloud.org/documents/3117825-Qui-Tam-Complaint-With-Exhibit-a-and-Attachments.html#document/p1',
+      ],
+    },
+    {
+      title: 'Trump signed a consent letter for $50 million deal between Bayrock and FL',
+      description: `The deal asked Trump to “indicate your consent to the Transaction as evidenced by the Transaction Documents by counterexecuting and returning to the undersigned a copy of this letter”. The main enclosed document was the loan agreement, which explicitly listed Sater. Mr Trump, Donald Jr and Ivanka were informed of the FL deal in advance and signed letters acknowledging the receipt of the information.`,
+      date: '7/16/2007',
+      nodes: ['Felix Slater', 'Donald Trump', 'Ivanka Trump', 'Donald Trump Jr.', 'The Trump Organization', 'Bayrock Group LLC', 'FL Group'],
+      sources: [
+        'https://www.ft.com/content/549ddfaa-5fa5-11e6-b38c-7b39cbb1138a',
+        'http://www.telegraph.co.uk/news/2016/05/26/exclusive-russian-mob-linked-fraudster-a-key-player-in-donald-tr/',
+      ],
+    },
+    {
+      title: `Trump gives sworn testimony claiming to not know Slater well`,
+      description: `Trump has repeatedly said he barely remembers Sater. In sworn testimony in 2013, Trump said he wouldn’t recognize Sater if they were sitting in the same room.`,
+      date: '11/5/2013',
+      nodes: ['Donald Trump', 'Felix Slater'],
+      sources: ['https://www.washingtonpost.com/politics/former-mafia-linked-figure-describes-association-with-trump/2016/05/17/cec6c2c6-16d3-11e6-aa55-670cabef46e0_story.html?utm_term=.aa555999a5ad'],
+    },
+    {
+      title: `Bayrock produces brochure naming the oligarch trio`,
+      description: `One of the listed partners is Alexander Machkevich and the Eurasia Group, which he controls with Chodiev and Ibragimov (the Trio). Exact date Brochure created is unknown, but had to be during the year of 2007 while Trump SoHo is being worked on.`,
+      date: '1/1/2007',
+      nodes: ['Alexander Machkevich', 'Bayrock Group LLC', 'Patokh Chodiev', 'Alijan Ibragimov'],
+      sources: ['https://www.forbes.com/sites/richardbehar/2016/10/03/trump-and-the-oligarch-trio/#2b5d6c165314']
+    },
   ],
 }
